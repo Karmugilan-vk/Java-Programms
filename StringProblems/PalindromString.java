d68a934db8f49d63e11b5743230dda77e01571bc
Karmugilan R <karmugilanrama@gmail.com>
2026-05-30 10:37:03 +0530

    Check the palindrom or not

---
+++ StringProblems/PalindromString.java
@@ -0,0 +1,16 @@
+public class PalindromString {
+    public static void main(String[] args){
+        String str = "markram";
+        String str1 = "";
+        for(int i = 0; i < str.length(); i++) {
+            str1 = str.charAt(i)+str1;
+        }
+        if(str.equals(str1)){
+            System.out.println("Palindrome");
+        }
+        else{
+            System.out.println("Not a Palindrome");
+        }
+    }
+    
+}
